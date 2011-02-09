b429842c69367281f2e3ae229845fb1c2ee38ba4
alkemann <alek.morland@gmail.com>
2011-02-09 23:15:48 +0100

    doing output buffering stuff in Render

--- lib/render.php
+++ lib/render.php
@@ -7,17 +7,28 @@ namespace core;
 class Render {
 
     public function head() {
+        ob_start();
         include LAYOUT_PATH . 'head.html.php';
         include LAYOUT_PATH . 'neck.html.php';
+        $ret = ob_get_contents();
+        ob_end_clean();
+        return $ret;
     }
 
     public function view() {
-        //include CONTENT_PATH . 'pages' . DS . 'start.html.php';
-        dd($_SERVER);
+        ob_start();
+        include CONTENT_PATH . 'pages' . DS . 'start.html.php';
+        $ret = ob_get_contents();
+        ob_end_clean();
+        return $ret;
     }
 
     public function foot() {
+        ob_start();
         include LAYOUT_PATH . 'foot.html.php';
+        $ret = ob_get_contents();
+        ob_end_clean();
+        return $ret;
     }
 
 }
