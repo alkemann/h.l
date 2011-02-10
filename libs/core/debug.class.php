cedcb01018915ec0e15b730f0a37e97d82169a48
alkemann <alek.morland@gmail.com>
2011-02-11 00:09:43 +0100

    allowing depth setting of the dump

--- libs/core/debug.class.php
+++ libs/core/debug.class.php
@@ -6,9 +6,15 @@
 class Debug {
 
     public static function dump($var, $options = array()) {
-        $options += array('trace' => null, 'echo' => true, 'die' => false);
+        $options += array(
+            'trace' => null,
+            'echo' => true,
+            'depth' => 6 
+        );
+        
         if (!$options['trace']) $options['trace'] = debug_backtrace();
         extract($options);
+        ini_set('xdebug.var_display_max_depth', $depth);
         $location = Debug::location($trace);
         ob_start();
         var_dump($var);
@@ -18,8 +24,7 @@ class Debug {
         $locString = Debug::locationString($location);
         $result = '<div style="font-size:11px;">' . $locString . $dump . '</div>';
         if ($echo) echo $result;
-        if ($die) die('<div style="margin-top: 25px;font-size: 10px;color: #500;">-Debug die-</div>');
-    }
+   }
 
     private static function locationString($location) {
         extract($location);
