aa1d8f483b2ab7d2cfe1cbf7f0a25963e5894572
alkemann <alek.morland@gmail.com>
2011-02-10 23:14:17 +0100

    okey that was just silly now..

--- webroot/index.php
+++ webroot/index.php
@@ -16,7 +16,7 @@ define('LIBS_PATH', ROOT . DS . LIB_DIR);
 define('LOGS_PATH', ROOT . DS . RESOURCE_DIR . DS . 'logs' . DS);
 
 define('DEBUG', 1);
-if ('DEBUG') require CORE_PATH . 'core' . DS . 'debug.class.php';
+if (DEBUG) require CORE_PATH . 'core' . DS . 'debug.class.php';
 
 define('CLASS_DIR', LIBS_PATH); // USED BY SPL_AUTOLOADER
 set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
