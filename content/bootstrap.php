90c13d475698f9fda18d9f899ce4a3c620df692d
Alexander Morland <alek@redpill-linpro.com>
2011-02-11 10:15:47 +0100

    refactored url_route

--- content/bootstrap.php
+++ content/bootstrap.php
@@ -2,10 +2,14 @@
 
 // Set up custom routes
 function url_route($requestUrl) {
+
+    $requestUrl = is_array($requestUrl) && isset($requestUrl['url']) ? $requestUrl['url'] : '/';
+    $requestUrl = rtrim($requestUrl, '/');
+    $requestUrl = '/' . $requestUrl;
     $aliases = array(
         '/' => 'home',
-        '/some/thing/deep' => 'start'
+        '/start' => 'start/something/deep'
     );
     
-    return isset($aliases['/' . $requestUrl]) ? $aliases['/' . $requestUrl] : $requestUrl;
+    return isset($aliases[$requestUrl]) ? $aliases[$requestUrl] : $requestUrl;
 }
