ad320ecdc38f6ad35076b0db5458715d398b6e3a
Alexander Morland <alek@redpill-linpro.com>
2011-02-11 10:41:01 +0100

    slight change to defined paths

--- webroot/index.php
+++ webroot/index.php
@@ -4,19 +4,20 @@ define('CONTENT_DIR', 'content');
 define('LAYOUT_DIR', 'layout');
 define('LIB_DIR', 'libs');
 define('RESOURCE_DIR', 'resources');
+define('CORE_DIR', 'core');
 //** auto defines
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(dirname(__FILE__))));
 define('WEBROOT_DIR', basename(dirname(__FILE__)));
 define('WWW_ROOT', dirname(__FILE__) . DS);
-define('CORE_PATH', ROOT . DS . LIB_DIR . DS);
 define('CONTENT_PATH', ROOT . DS . CONTENT_DIR . DS);
 define('LAYOUT_PATH', CONTENT_PATH . LAYOUT_DIR . DS);
-define('LIBS_PATH', ROOT . DS . LIB_DIR);
+define('LIBS_PATH', ROOT . DS . LIB_DIR . DS);
+define('CORE_PATH', LIBS_PATH . CORE_DIR . DS);
 define('LOGS_PATH', ROOT . DS . RESOURCE_DIR . DS . 'logs' . DS);
 
 define('DEBUG', 1);
-if (DEBUG) require CORE_PATH . 'core' . DS . 'debug.class.php';
+if (DEBUG) require CORE_PATH . 'debug.class.php';
 
 define('CLASS_DIR', LIBS_PATH); // USED BY SPL_AUTOLOADER
 set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
