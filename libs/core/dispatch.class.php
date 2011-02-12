776f1debd6808249a91352efccae79e90251c6d9
alkemann <alek.morland@gmail.com>
2011-02-12 17:16:17 +0100

    simplified Dispatch

--- libs/core/dispatch.class.php
+++ libs/core/dispatch.class.php
@@ -6,21 +6,13 @@ namespace core;
 
 class Dispatch {
     
-    protected $_render;
-    protected $_requestObj;
-
-    public function render() {
-        return $this->_render;
-    }
-
-    public function request() {
-        return $this->_requestObj;
-    }
+    public $_render;
+    public $_requestObj;
 
     public function run() {
         $this->_requestObj = new Request();
         $this->_render = new Render($this->_requestObj);
-        return $this->_render->render();
+        $this->_render->render();
     }
 
 }
