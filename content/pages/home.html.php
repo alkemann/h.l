707b120aa5b22c4bb91ee0a0f6e160223e4741a3
alkemann <alek.morland@gmail.com>
2011-02-12 12:39:38 +0100

    dump on home

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -1,10 +1,7 @@
 <h1> Start page</h1>
 
 <h2>Defines are : </h2>
-
-<dl>
 <?php
-
 $defines = array();
 $defines['CONTENT_DIR'] = CONTENT_DIR;
 $defines['LAYOUT_DIR'] = LAYOUT_DIR;
@@ -16,6 +13,8 @@ $defines['CORE_PATH'] = CORE_PATH;
 $defines['CONTENT_PATH'] = CONTENT_PATH;
 $defines['LAYOUT_PATH'] = LAYOUT_PATH;
 
-foreach ($defines as $dt => $dd) {
-	echo "<dt>$dt</dt><dd>$dd</dd>\n";
-}
+dd($defines);
+?>
+<h2>SERVER are :</h2>
+<?php
+dd($_SERVER);
