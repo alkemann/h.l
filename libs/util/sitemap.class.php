7cdfa5463445331a9022b0bc78906fd52ebf4438
alkemann <alek.morland@gmail.com>
2011-02-13 14:35:27 +0100

    fixed recursive sitemap function

--- libs/util/sitemap.class.php
+++ libs/util/sitemap.class.php
@@ -12,7 +12,7 @@ class Sitemap {
 
     private static function recursive($dir, $path) {
         $subfolders = array();
-        $files = array();
+        $ret = array();
         if (is_dir($path.$dir)) {
             if ($dirHandler = opendir($path.$dir)) {
                 while (($file = readdir($dirHandler)) !== false) {
@@ -20,13 +20,12 @@ class Sitemap {
                     if (filetype($path.$dir.DS.$file) == 'dir') {
                         $subfolders[] = $file;
                     } else {
-                        $files[] = $file;
+                        $ret[] = $file;
                     }
                 }
                 closedir($dirHandler);
-                $ret = array('.' => $files);
                 foreach ($subfolders as $folder) {
-                    $ret['.'][$folder] = static::recursive($folder, $path.$dir.DS);
+                    $ret[$folder] = static::recursive($folder, $path.$dir.DS);
                 }
             }
         }
