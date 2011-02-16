f5af299cdc03b6fda3a89f4bb1cad2bd804636cf
alkemann <alek.morland@gmail.com>
2011-02-16 22:58:33 +0100

    formatting trailing whitespace

--- libs/core/request.class.php
+++ libs/core/request.class.php
@@ -8,7 +8,7 @@ namespace core;
 use \core\Router;
 
 class Request {
-    
+
     protected $_url;
     protected $_fullUrl;
     protected $_type = 'html';
@@ -20,12 +20,12 @@ class Request {
 
     public function __construct() {
         $this->_url = Router::request_to_url($_REQUEST);
-        
+
         $parts = \explode('/', $this->_url);
         $last = \array_slice($parts, -1, 1, true);
         unset($parts[key($last)]);
         $this->_path = $parts;
-        
+
         $this->_view = current($last);
         if ($p = strrpos($this->_view, '.')) {
             $type = substr($this->_view, $p + 1);
@@ -33,9 +33,9 @@ class Request {
                 $this->_type = $type;
                 $this->_view = substr($this->_view, 0, $p);
             }
-        }        
+        }
     }
- 
+
     public function viewToRender() {
         $ret = join(DS, $this->path()) . DS .
             $this->view() . '.' .
@@ -43,11 +43,11 @@ class Request {
         $ret = trim($ret, DS);
         return $ret;
     }
-    
+
     public function path() { return $this->_path; }
     public function view() { return $this->_view; }
     public function type() { return $this->_type; }
-    
+
     public function canon() {
         $view = $this->viewToRender();
         $view = str_replace('\\', '/', $view);
