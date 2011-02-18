8025ca5189ceb76274162c7bc39706b09cc4a36b
Alexander Morland <alek@redpill-linpro.com>
2011-02-18 21:35:29 +0100

    using new debug globals in temp debug page

--- content/custom/debug.html.php
+++ content/custom/debug.html.php
@@ -2,10 +2,10 @@
 
 <h2>Defines are : </h2>
 <?php
-$debug = new \Debug();
+$debug = \util\Debug::get_instance();
 $defines = $debug->defines();
-dd($debug->defines());
+dw('defines');
 ?>
 <h2>SERVER are :</h2>
 <?php
-dd($_SERVER);
+d($_SERVER);
