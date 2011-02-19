44f373118a0920a4883383613a0191fdbfe4e9f1
Alexander Morland <alek@redpill-linpro.com>
2011-02-19 11:17:10 +0100

    ucfirst on sitemap names, use a string util

--- content/custom/sitemap.html.php
+++ content/custom/sitemap.html.php
@@ -14,21 +14,21 @@ $sitemap = \util\Sitemap::generate();
         foreach ($sitemap as $name => $contains) {
             echo '<li>';
             if (is_string($contains[0]))
-                echo '<a href="' . $contains[0] . '" >' . $name . '</a>';
+                echo '<a href="' . $contains[0] . '" >' . ucfirst($name) . '</a>';
             $children = (isset($contains[1])) ? $contains[1] : $contains[0];
             if (is_array($children)) {
                 echo '<ul>';
                 foreach ($children as $name1 => $contains1) {
                     echo '<li>';
                     if (is_string($contains1[0]))
-                        echo '<a href="/' . $name . '/' . $contains1[0] . '" >' . $name1 . '</a>';
+                        echo '<a href="/' . $name . '/' . $contains1[0] . '" >' . ucfirst($name1) . '</a>';
                     $children1 = (isset($contains1[1])) ? $contains1[1] : $contains1[0];
                     if (is_array($children1)) {
                         echo '<ul>';
                         foreach ($children1 as $name2 => $contains2) {
                             echo '<li>';
                             if (is_string($contains2[0]))
-                                echo '<a href="/' . $name . '/' . $name1 . $contains2[0] . '" >' . $name2 . '</a>';
+                                echo '<a href="/' . $name . '/' . $name1 . $contains2[0] . '" >' . ucfirst($name2) . '</a>';
                             echo '</li>';
                         }
                         echo '</ul>';
