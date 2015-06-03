1303b406f1d19f2d0fea573bc1445b683974d3a2
Alexander Morland <alek.morland@gmail.com>
2015-06-03 18:28:25 +0200

    Removed all app specific and started the process of turning only the h.l part into a composer lib

--- content/layout/neck.html.php
+++
@@ -1,3 +0,0 @@
- <link rel="canonical" href="<?php echo $render->request()->canon(); ?>" />
-</head>
-<body>
--- resources/configs/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use hl\core\Router;
-
-Router::alias('/', 'home');
-Router::alias('/index', 'home');
-Router::alias('/start', 'some/thing/deep');
-Router::alias('/place', 'some/thing/deep');
