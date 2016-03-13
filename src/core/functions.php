2ab6f3d567f9cdb24058aba2dbdc97245927ea39
Alexander Morland <alek.morland@gmail.com>
2016-03-13 18:25:10 +0100

    fixed error handler's several bugs

--- src/core/functions.php
+++ src/core/functions.php
@@ -2,30 +2,30 @@
 
 namespace alkemann\hl\core;
 
+use alkemann\hl\core\exceptions\InvalidUrlException;
+
 function handleError(\Exception $exception, Request $request) {
-        if ($exception instanceof alkemann\hl\core\exceptions\InvalidUrlException) {
-            header("HTTP/1.0 404 Not Found");
-            $render = $dispatch->_render;
-            $request->setRequestAs404();
-            $render->setData('missing', $exception->getMessage());
-            $render->render();
-        } elseif (DEBUG === 0) {
-            $file = LOGS_PATH . 'error.' . time() . '.log';
-            $fileHandler = fopen($file, 'c');
-            $exceptionMessage = strip_tags($exception->__toString());
-            fwrite($fileHandler, $exceptionMessage);
-            fwrite($fileHandler, "\n\n");
-            fwrite($fileHandler, print_r($dispatch->getRequest(), true));
-            fclose($fileHandler);
-            header("HTTP/1.0 500 Interal Server Error");
+    if ($exception instanceof InvalidUrlException) {
+        header("HTTP/1.0 404 Not Found");
+        $render = new Page($request);
+        $request->setRequestAs404();
+        $render->setData('missing', $exception->getMessage());
+        $render->render();
+    } elseif (DEBUG === 0) {
+        $file = LOGS_PATH . 'error.' . time() . '.log';
+        $fileHandler = fopen($file, 'c');
+        $exceptionMessage = strip_tags($exception->__toString());
+        fwrite($fileHandler, $exceptionMessage);
+        fwrite($fileHandler, "\n\n");
+        fwrite($fileHandler, print_r($request, true));
+        fclose($fileHandler);
+        header("HTTP/1.0 500 Interal Server Error");
 
-            $errorPage = CONTENT_PATH . 'custom' . DS . 'error500.html.php';
-            if (file_exists($errorPage)) {
-                include $errorPage;
-            }
-        } else {
-            $render = new Page($request);
-            $render->render((string) $exception);
+        $errorPage = CONTENT_PATH . 'custom' . DS . 'error500.html.php';
+        if (file_exists($errorPage)) {
+            include $errorPage;
         }
-
+    } else {
+        (new Page($request))->render((string) $exception);
+    }
 }
