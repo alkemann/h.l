47c2b90de735a871273bdb1b9a11f47802976496
Alexander Morland <alek.morland@gmail.com>
2016-03-19 16:46:24 +0100

    Cleaned up and formatted the page vs dynamic routing

--- src/core/Request.php
+++ src/core/Request.php
@@ -16,66 +16,41 @@ class Request {
     const DELETE = 'DELETE';
 
     protected $_url;
-    protected $_fullUrl;
-    protected $_type = 'html';
-    protected $_validTypes = array('html','json', 'xml');
-    protected $_contentTypes = array('html' => 'text/html', 'json' => 'application/json', 'xml' => 'application/xml');
-    protected $_static;
-    protected $_webroot;
     protected $_path;
     protected $_view;
     protected $_method;
     protected $_route;
+    protected $_type = 'html';
+    protected $_validTypes = ['html','json', 'xml'];
+    protected $_contentTypes = [
+        'html' => 'text/html',
+        'json' => 'application/json',
+        'xml' => 'application/xml'
+    ];
 
     public function __construct() {
-        $this->_method = $_SERVER['REQUEST_METHOD'];
         $this->setResponseTypeFromHeaders();
-
-        $route = Router::request_to_url($_REQUEST, $_SERVER['REQUEST_METHOD']);
-
-        if ($route instanceof Route) {
-            $this->_url = $_REQUEST['url'];
-            $this->_route = $route;
-        } else {
-            $this->_url = $route;
-            $this->setPageFromUrl();
-        }
+        $this->_method  = $_SERVER['REQUEST_METHOD'];
+        $route          = Router::match($_REQUEST, $_SERVER['REQUEST_METHOD']);
+        $this->_url     = $route->url;
+        $this->_route   = $route;
     }
 
-    private function setPageFromUrl() {
-        $parts = \explode('/', $this->_url);
-        $last = \array_slice($parts, -1, 1, true);
-        unset($parts[key($last)]);
-        $this->_path = $parts;
-        $this->_view = current($last);
-
-        $period = strrpos($this->_view, '.');
-        if ($period) {
-            $type = substr($this->_view, $period + 1);
-            if (in_array($type, $this->_validTypes)) {
-                $this->_type = $type;
-                $this->_view = substr($this->_view, 0, $period);
-            }
-        }
-    }
-
-    public function viewToRender() {
-        $ret = join(DS, $this->path()) . DS .
-            $this->view() . '.' .
-            $this->type();
-        $ret = trim($ret, DS);
-        return $ret;
-    }
-
-    public function getPostBody() {
-        return file_get_contents('php://input');
-    }
-
+    public function getPostBody() { return file_get_contents('php://input'); }
+    public function route() { return $this->_route; }
+    public function url() { return $this->_url; }
     public function method() { return $this->_method; }
     public function path() { return $this->_path; }
+    public function setPath($path) { $this->_path = $path; }
     public function view() { return $this->_view; }
+    public function setView($view) { $this->_view = $view; }
     public function type() { return $this->_type; }
-    public function validType() { return in_array($this->type(), $this->_validTypes); }
+
+    public function validType($type = null) {
+        if (is_null($type)) $type= $this->type();
+        return in_array($type, $this->_validTypes);
+    }
+
     public function contentType() {
         if ($this->validType()) {
             return $this->_contentTypes[$this->type()];
@@ -110,6 +85,14 @@ class Request {
         return $base . $view;
     }
 
+    public function viewToRender() {
+        $ret = join(DS, $this->path()) . DS .
+            $this->view() . '.' .
+            $this->type();
+        $ret = trim($ret, DS);
+        return $ret;
+    }
+
     public function setRequestAs404() {
         $this->_view = 'error404';
         $this->_path = [];
@@ -123,12 +106,9 @@ class Request {
     }
 
     public function response() {
-        if ($this->_route) {
-            $cb = $this->_route->callback;
-            $this->_parameters = $this->_route->parameters;
-            return call_user_func_array($cb, [$this]);
-        }
-        return new Page($this);
+        $cb = $this->_route->callback;
+        $this->_parameters = $this->_route->parameters;
+        return call_user_func_array($cb, [$this]);
     }
 
     // TODO add support for reverse route match
--- src/core/Router.php
+++ src/core/Router.php
@@ -7,26 +7,8 @@ class Router {
     protected static $_aliases = [];
     protected static $_routes = [];
 
-    public static function request_to_url($requestUrl, $method = [Request::GET]) {
-        $requestUrl = is_array($requestUrl) && isset($requestUrl['url']) ? $requestUrl['url'] : '/';
-
-        $cb = static::match($requestUrl, $method);
-        if ($cb) {
-            return $cb;
-        }
-
-        $requestUrl = rtrim($requestUrl, '/');
-        if (strrpos($requestUrl, '.html')) $requestUrl = substr($requestUrl, 0, -5);
-        $requestUrl = '/' . $requestUrl;
-        return isset(static::$_aliases[$requestUrl]) ? static::$_aliases[$requestUrl] : $requestUrl;
-    }
-
     public static function alias($alias, $real) {
         static::$_aliases[$alias] = $real;
-    }
-
-    public static function aliases() {
-        return static::$aliases;
     }
 
     public static function add($url, \Closure $closure, $methods = [Request::GET]) {
@@ -35,18 +17,51 @@ class Router {
     }
 
     public static function match($url, $method = Request::GET) {
-        if (!isset(static::$_routes[$method])) return false;
-        foreach (static::$_routes[$method] as $route => $cb) {
-            if ($url === $route) return $cb;
-            $result = preg_match($route, $url, $matches);
-            if (!$result) continue;
+        $url = is_array($url) && isset($url['url']) ? $url['url'] : '/';
+        if (isset(static::$_routes[$method])) {
+            // Look for dynamic routes
+            foreach (static::$_routes[$method] as $route => $cb) {
+                if ($url === $route) return $cb;
+                $result = preg_match($route, $url, $matches);
+                if (!$result) continue;
 
             $parameters = array_filter($matches, function($v) {return !is_int($v);}, 
                 ARRAY_FILTER_USE_KEY);
 
-            return new Route($url, $route, $cb, $parameters);
+                return new Route($url, $route, $cb, $parameters);
 
+            }
         }
-        return false;
+        // Not a dynamic route, assume static route
+        $routeUrl = static::convertRequestUrlToPageUrl($url);
+        return new Route($routeUrl, $url, function($request) {
+            // unpack the url and convert to a page file path
+            // set the view on the request then return a Page
+            $url = $request->url();
+            $parts = \explode('/', $url);
+            $last = \array_slice($parts, -1, 1, true);
+            unset($parts[key($last)]);
+            $request->setPath($parts);
+            $view = current($last);
+            $request->setView($view);
+            $period = strrpos($view, '.');
+            if ($period) {
+                $type = substr($view, $period + 1);
+                if ($request->validType($type)) {
+                    $request->overrideResponseType($type);
+                    $view = substr($view, 0, $period);
+                    $request->setView($view);
+                }
+            }
+            return new Page($request);
+        });
+    }
+
+    private static function convertRequestUrlToPageUrl($url) {
+        $url = rtrim($url, '/');
+        if (strrpos($url, '.html'))
+            $url = substr($url, 0, -5);
+        $url = '/' . $url;
+        return isset(static::$_aliases[$url]) ? static::$_aliases[$url] : $url;
     }
 }
